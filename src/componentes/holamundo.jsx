cee0e5e5476a94b8f2ae7c33c4ce58bdf063bd72
ALONSO ORLANDO JORQUERA TAPIA <alonso.jorquera@inacapmail.cl>
2023-07-10 18:20:40 -0400

    añadidas rutas y componentes

---
+++ src/componentes/holamundo.jsx
@@ -0,0 +1,14 @@
+import react,{useState, useEffect} from 'react'
+
+// function HolaMundo = {}
+//view//
+const HolaMundo = () => {
+    
+    return (
+        <>
+        <h1>Hola Mundo</h1>
+        {/*<p className="algo">Diga algo</p>*/}
+        </>
+    )
+}
+ export default HolaMundo 
